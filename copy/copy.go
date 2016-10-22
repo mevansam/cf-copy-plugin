ff881f5e5019e299ecc144fe40dba7417b1aea8d
Mevan Samaratunga <mevansam@gmail.com>
2016-10-22 23:53:58 +0400

    added classes to better isolate download and upload of app buts

--- copy/copy.go
+++ copy/copy.go
@@ -1,5 +1,10 @@
 package copy
 
+import (
+	"code.cloudfoundry.org/cli/cf/models"
+	"github.com/mevansam/cf-copy-plugin/helpers"
+)
+
 // ApplicationsManager -
 type ApplicationsManager interface {
 	ApplicationsToBeCopied(appNames []string) (ApplicationCollection, error)
@@ -14,11 +19,23 @@ type ApplicationCollection interface {
 // ServicesManager -
 type ServicesManager interface {
 	ServicesToBeCopied(appNames []string, upsServices []string) (ServiceCollection, error)
-	DoCopy(services ServiceCollection) error
+	DoCopy(services ServiceCollection, recreate bool) error
 	Close()
 }
 
 // ServiceCollection -
 type ServiceCollection interface {
 	AppBindings(appName string) ([]string, bool)
+}
+
+// ApplicationContentProvider -
+type ApplicationContentProvider interface {
+	NewApplicationDroplet(session helpers.CloudControllerSession) ApplicationContent
+	NewApplicationBits(session helpers.CloudControllerSession) ApplicationContent
+}
+
+// ApplicationContent -
+type ApplicationContent interface {
+	Download(app models.Application) error
+	Upload(params models.AppParams) error
 }
