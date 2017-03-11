7f626a5b1d77feb43ac51899c8107de89712f2b0
Mevan Samaratunga <mevansam@gmail.com>
2017-03-11 10:09:41 +0400

    extract version from argument if not building from tag

--- build-docker.sh
+++ build-docker.sh
@@ -6,17 +6,17 @@ set -x
 TAG="$(git tag -l --points-at HEAD)"
 if [[ -z "$TAG" ]]; then
     TAG=$4
-else
-	MAJOR=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $1;}'`
-	MINOR=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $2;}'`
-	BUILD=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $3;}'`
-
-	sed -i.bak1 "s/Major:.*/Major: $MAJOR,/" command/copy_plugin.go
-    sed -i.bak2 "s/Minor:.*/Minor: $MINOR,/" command/copy_plugin.go
-    sed -i.bak3 "s/Build:.*/Build: $BUILD,/" command/copy_plugin.go
-
-    echo "Building docker image with plugin release $TAG."
 fi
+
+MAJOR=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $1;}'`
+MINOR=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $2;}'`
+BUILD=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $3;}'`
+
+sed -i.bak1 "s/Major:.*/Major: $MAJOR,/" command/copy_plugin.go
+sed -i.bak2 "s/Minor:.*/Minor: $MINOR,/" command/copy_plugin.go
+sed -i.bak3 "s/Build:.*/Build: $BUILD,/" command/copy_plugin.go
+
+echo "Building docker image with plugin release $TAG."
 
 if [[ -n "$TAG" ]] ; then
     GOOS=linux GOARCH=amd64 go build
