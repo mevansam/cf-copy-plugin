416f9284a524e46b9a0577612c7f337c3b691ae3
Mevan Samaratunga <mevansam@gmail.com>
2017-03-11 09:18:10 +0400

    finalized docker build script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM alpine:latest
-ARG VER
 
 COPY bin/linux64/cf-copy-plugin /usr/local/bin/ 
 
@@ -8,5 +7,5 @@ RUN apk --no-cache add curl
 RUN curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.25.0&source=github-rel" | tar xz -C /usr/local/bin/ cf
 RUN cf install-plugin -f -r CF-Community "Targets"
 
-RUN curl -L "https://github.com/mevansam/cf-copy-plugin/releases/download/$VER/cf-copy-plugin_linux64.tar.gz" | tar xz -C /usr/local/bin/ cf-copy-plugin
+COPY cf-copy-plugin /usr/local/bin/
 RUN cf install-plugin -f /usr/local/bin/cf-copy-plugin && rm /usr/local/bin/cf-copy-plugin
--- build-docker.sh
+++ build-docker.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
 set -e
+set -x
 
 TAG="$(git tag -l --points-at HEAD)"
-if [[ -n "$TAG" ]] ; then
-
+if [[ -z "$TAG" ]]; then
+    TAG=$4
+else
 	MAJOR=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $1;}'`
 	MINOR=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $2;}'`
 	BUILD=`echo $TAG |  awk 'BEGIN {FS = "." } ; { printf $3;}'`
 
-	`sed -i -e "s/Major:.*/Major: $MAJOR,/" \
-		-e "s/Minor:.*/Minor: $MINOR,/" \
-		-e "s/Build:.*/Build: $BUILD,/" command/copy_plugin.go`
-else
-    TAG=$1
+	sed -i.bak1 "s/Major:.*/Major: $MAJOR,/" command/copy_plugin.go
+    sed -i.bak2 "s/Minor:.*/Minor: $MINOR,/" command/copy_plugin.go
+    sed -i.bak3 "s/Build:.*/Build: $BUILD,/" command/copy_plugin.go
+
+    echo "Building docker image with plugin release $TAG."
 fi
 
 if [[ -n "$TAG" ]] ; then
-    docker build . -t cf-cli-copy-release --squash --build-arg VER=$TAG
+    GOOS=linux GOARCH=amd64 go build
+    docker build . -t cf-cli-copy-release --squash
 
-    if [[ -n $3 ]] && [[ -n $4 ]]; then
-        docker login -u $3 -p $4
+    cp command/copy_plugin.go.bak1 command/copy_plugin.go
+    rm command/copy_plugin.go.bak*
+    rm -f cf-copy-plugin
 
-        docker tag cf-cli-copy-release $2/cf-cli-copy:latest
-        docker tag cf-cli-copy-release $2/cf-cli-copy:$TAG
-        docker push $2/cf-cli-copy
+    if [[ -n $2 ]] && [[ -n $3 ]]; then
+        docker login -u $2 -p $3
+
+        docker tag cf-cli-copy-release $1/cf-cli-copy:latest
+        docker tag cf-cli-copy-release $1/cf-cli-copy:$TAG
+        docker push $1/cf-cli-copy
 
         # clean up
-        docker rmi $2/cf-cli-copy:latest
-        docker rmi $2/cf-cli-copy:$TAG
+        docker rmi $1/cf-cli-copy:latest
+        docker rmi $1/cf-cli-copy:$TAG
         docker rmi cf-cli-copy-release
     fi
-	exit 0
 fi
 
+set +x
 set +e
